6525b9bfa7bb321ddf94eb9f58a3285aa4a84061
Ayan Das <ayandas9531@gmail.com>
2023-03-05 19:22:46 +0530

    used shared preferences for project

--- social_app/lib/ThemeManager.dart
+++ social_app/lib/ThemeManager.dart
@@ -1,15 +1,33 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeManger with ChangeNotifier {
+  ThemeManger() {
+    getPrefs();
+  }
+
   ThemeMode _themeMode = ThemeMode.light;
 
   ThemeMode get themeMode => _themeMode;
 
-  void toggleTheme() {
-    _themeMode == ThemeMode.light
-        ? _themeMode = ThemeMode.dark
-        : _themeMode = ThemeMode.light;
+  void toggleTheme() async {
+    bool isDark = _themeMode == ThemeMode.dark;
+    isDark ? _themeMode = ThemeMode.light : _themeMode = ThemeMode.dark;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDark', !isDark);
+  }
+
+  getPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool? isDark = prefs.getBool('isDark');
+    if (isDark == null) {
+      isDark = false;
+      await prefs.setBool('isDark', isDark);
+    }
+    isDark ? _themeMode = ThemeMode.dark : ThemeMode.light;
+    print(prefs.getBool('isDark'));
     notifyListeners();
   }
 }
